14279eb4816a496831e1806e6d7fe2fb01f1154d
Juaan26 <Juaan26g@gmail.com>
2025-02-21 20:27:28 +0100

    barra de busqueda provisionalmente funcional

--- ecommerce-demo/src/components/Search.jsx
+++ ecommerce-demo/src/components/Search.jsx
@@ -1,10 +1,34 @@
 import './Search.css'
+import { products } from '../mocks/products.json'
+import { useState } from 'react'
 export const Search = () => {
+    const [search, setSearch] = useState('')
+    const [filteredProducts, setFilteredProducts] = useState([])
+
+    const handleFilter = () => {
+        if (search === '') {
+            setFilteredProducts([])
+            return
+        }
+        const filtered = products.filter(product => product.title.toLowerCase().includes(search.toLowerCase()))
+        setFilteredProducts(filtered)
+
+    }
 
     return (
         <div className="search">
-            <input className="search-input" type="text" placeholder="Search..." />
-            <button className="search-button">Search</button>
+            <input className="search-input" type="text" placeholder="Search..." value={ search } onChange={ e => setSearch(e.target.value) } />
+            <button onClick={ handleFilter } className="search-button">Search</button>
+            <ul className={ `search-results ${filteredProducts.length === 0 ? 'hidden' : ''}` }>
+                {
+                    filteredProducts.map(product => (
+                        <li key={ product.id }>
+                            <img className='search-images' src={ product.thumbnail } alt={ product.title } />
+                            { product.title }
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 
